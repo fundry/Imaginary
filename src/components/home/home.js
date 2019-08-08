d0fb6eb006e41df514e304a478f125f0965ea80d
Nwani Victory <vickywane@gmail.com>
2019-08-08 03:46:02 +0100

    Update

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>HOME COMPONENT</Text>
+    </View>
+  );
+};
+
+export default Home;
